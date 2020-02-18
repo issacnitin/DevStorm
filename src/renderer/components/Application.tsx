d7c3b16f69f0120a9acc44195520f4a9a30cfb75
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-02-18 19:53:52 +0530

    How to execute and install 101 for unix

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -2,12 +2,44 @@ import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
 import CounterContainer from '../containers/CounterContainer';
+import * as sudo from 'sudo-prompt';
+import * as process from 'child_process';
 
-const Application = () => (
-    <div>
-        Hello World from Electron!
-        <CounterContainer />
-    </div>
-);
+var options = {
+  name: 'Electron'
+};
+
+var brewInstalled = false;
+
+const Application = () => {
+    
+    process.exec('./scripts/unix/nginx/install.sh',
+        function(error: any, stdout: any, stderr: any) {
+            if (error) {
+                process.exec("./scripts/unix/brew/install.sh", (error: any, stdout: any, stderr: any) => {
+                    
+                        if(error) throw error;
+                        console.log(stdout);
+                    }
+                )
+            } else {
+
+                process.exec('./scripts/unix/nginx/install.sh', 
+                (error: any, stdout: any, stderr: any) => {
+                    if(error) throw error;
+                    console.log(stdout);
+                }
+                );
+                console.log('stdout: ' + stdout);
+            }
+        }
+    );
+    return (
+        <div>
+            Hello World from Electron!
+            <CounterContainer />
+        </div>
+    )
+}; 
 
 export default hot(Application);
