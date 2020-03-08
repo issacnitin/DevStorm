da57036e6e2200a131456ef797241a57c2006140
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-03-08 23:20:45 +0530

    Adding react-bootstrap

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -5,6 +5,7 @@ import { AppContainer } from 'react-hot-loader';
 
 import { Application } from './components/Application';
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Create main element
 const mainElement = document.createElement('div');
